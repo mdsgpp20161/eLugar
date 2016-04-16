2ede69c9eed621c49e3b3fa17866654da4ce27e5
Mateus Andrade <mateustwofs@gmail.com>
2016-04-16 17:04:10 -0300

    Listando as cidades alfabeticamente na listagem das cidades na pagina inicial e nos resultados da pesquisa

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,9 +2,9 @@ class CitiesController < ApplicationController
   
   def index
   	if(params[:find])
-  		@cities = City.where("name like ?", "%#{params[:find]}%")
+  		@cities = City.where("name like ?", "%#{params[:find]}%").sort{ |a,b| a.name.downcase <=> b.name.downcase }
   	else
-  		@cities = City.all.order('created_at DESC')
+  		@cities = City.all.sort{ |a,b| a.name.downcase <=> b.name.downcase }
   	end
   end
 
