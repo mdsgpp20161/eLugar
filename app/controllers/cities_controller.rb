8099cb04a3db447b2168eb0b534d159a2335c892
Felipe <wowaccount123555@hotmail.com>
2016-04-16 15:29:46 -0300

    Criada a funcionalidade procurar por parametro, e atualizado .gitignore Signed-off-by: Felipe Osório <felipe.osorio72@outlook.com.br> Signed-off-by: Harrison Pedro <hpedro1195@gmail.com>

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,9 @@
 class CitiesController < ApplicationController
   def index
+  	if(params[:find])
+  		@cities = City.where("name like ?", "%#{params[:find]}%")
+  	else
+  		@cities = City.all.order('CREATED AT DESC')
+  	end
   end
 end
