e7cc5429115a8d269756c7f6ba3f66a6b1ba2dac
Harrison <hpedro1195@gmail.com>
2016-04-30 23:31:50 -0300

    Alguns testes realizados, mas falta algumas funcionalidades a serem testadas

--- test/controllers/cities_controller_test.rb
+++ test/controllers/cities_controller_test.rb
@@ -5,6 +5,7 @@ class CitiesControllerTest < ActionController::TestCase
 
   def setup
 	@cityBrasilia = cities(:brasilia)
+  @cityCeara = cities(:ceara)
   end
 
   test "should get index" do
@@ -18,5 +19,32 @@ class CitiesControllerTest < ActionController::TestCase
   	assert_response :success
 
   end
+
+  test "should get show_att" do
+    get :show_cities, name: @cityBrasilia.name
+    get :show_cities, population: @cityBrasilia.population
+    get :show_cities, demographic_density: @cityBrasilia.demographic_density
+    assert_response :success
+  end
+
+  test "search city" do
+    get :show_cities, :find => 'brasilia'
+    assert_not_nil assigns(:cities)
+  end 
+
+  test "should not save city without any attribute" do
+      assert_not @cityCeara.save
+  end
+
+test "should get compare" do
+  get :compare, id: @cityBrasilia.id
+  assert_response :success
+end
+
+#test "should get compare from show" do
+#  get :compare, id: @cityBrasilia.id
+#  assert_response :success
+#  assert_match /https:\/\/localhost:3000\/compare/, @response.redirect_url
+#end
 
 end
