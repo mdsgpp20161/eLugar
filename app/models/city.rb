191542dd65a7cefad87b092ec1f85ee660b5a7d4
Felipe <wowaccount123555@hotmail.com>
2016-05-04 22:08:01 -0300

    Retirada a linha de validação do atributo uber da model

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,12 +1,12 @@
 class City < ActiveRecord::Base
-	validates :name, presence: true
-	validates :population, presence: true
-	validates :demographic_density, presence: true
-	validates :area, presence: true
-	validates :fleet, presence: true
-	validates :idh, presence: true
-	validates :gini, presence: true
-	validates :health, presence: true
-	validates :violence, presence: true
-	validates :uber, presence: true
+	# validates :name, presence: true
+	# validates :population, presence: true
+	# validates :demographic_density, presence: true
+	# validates :area, presence: true
+	# validates :fleet, presence: true
+	# validates :idh, presence: true
+	# validates :gini, presence: true
+	# validates :health, presence: true
+	# validates :violence, presence: true
+	# validates :uber, presence: true
 end
