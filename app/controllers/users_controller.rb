2148cbcfa5561e269eb9122d544eb2b20a97a422
Harrison <hpedro1195@gmail.com>
2016-05-29 10:16:40 -0300

    Deletar, mostrar perfil e editar criados e funcionando. Falta editar o layout dos mesmos.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
   def new
   	@user = User.new
   end
@@ -10,10 +12,29 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
   		redirect_to @user
   	else
   		render 'new'
   	end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to root_url
   end
 
   private
@@ -22,5 +43,16 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
    end
+
+   def logged_in_user
+      unless logged_in?
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+    end
 
 end
