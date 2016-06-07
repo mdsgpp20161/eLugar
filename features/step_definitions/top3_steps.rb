da91dcb606975301ca51fa2650b2996133f25401
Mateus Andrade <mateusandrade080@gmail.com>
2016-06-06 21:06:40 -0300

    Iniciando testes de aceitação da feature top3 Signed-off-by: Mateus Andrade <mateusandrade080@gmail.com> Signed-off-by: Gustavo Lopes <gustavo.ldbrito@gmail.com>

---
+++ features/step_definitions/top3_steps.rb
@@ -0,0 +1,19 @@
+Given(/^there is a data about top (\d+) city$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I see an information that is top (\d+) in Brazil$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I must see a gold medal close to the information$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I must see a silver medal close to the information$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I must see a bronze medal close to the information$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
