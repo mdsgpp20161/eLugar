76d3e5585c94e42d2d6b497526919e58caf69a74
Harrison <hpedro1195@gmail.com>
2016-06-07 17:27:04 -0300

    Corrigido o bug do metodo flash apos ter dado merge de branch.

--- db/schema.rb
+++ db/schema.rb
@@ -29,12 +29,6 @@ ActiveRecord::Schema.define(version: 20160529145244) do
     t.datetime "updated_at",          null: false
   end
 
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "access_token"
-
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
     t.text     "body"
@@ -48,7 +42,7 @@ ActiveRecord::Schema.define(version: 20160529145244) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "provider"
+    t.string   "access_token"
     t.string   "uid"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
