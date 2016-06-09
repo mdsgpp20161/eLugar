a3542e5003612223881f2a00ef702c8c83d70df0
Mateus Andrade <mateusandrade080@gmail.com>
2016-06-09 11:45:11 -0300

    Corrgido testes de aceitação da feature ranking

--- features/step_definitions/ranking_steps.rb
+++ features/step_definitions/ranking_steps.rb
@@ -2,12 +2,12 @@ Given(/^there is a option to choose attribute to show ranking$/) do
 		visit('/ranking')
 end
 
-When(/^I choose an option$/) do
+When(/^I choose the population option$/) do
 		select('População Estimada', :from => 'sort_cities')
 end
 
 When(/^Click to show the ranking$/) do
-		click_on('View Ranking')
+		click_on('view_ranking')
 end
 
 Then(/^I must see the ranking table$/) do
