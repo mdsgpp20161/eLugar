b2a2bf48423539db2cad5e43542532c23efd46fb
Harrison <hpedro1195@gmail.com>
2016-06-13 21:59:55 -0300

    Arrumado o bug de nao poder acessar as cidades se nao estivesse logado, falta modularizar o codigo. Signed-off-by: Harrison <hpedro1195@gmail.com> Signed-off-by: Gustavo <gustavo.ldbrito@gmail.com>

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,5 +1,6 @@
 class CitiesController < ApplicationController
   include CitiesHelper
+  include SessionsHelper
 
   def index
   end
@@ -108,85 +109,120 @@ class CitiesController < ApplicationController
 
   helper_method :get_emoji
   def get_emoji (attr_name, attr_value)
-    @profileQuiz = ProfileQuiz.new
-    @current_user = User.find_by(id: session[:user_id])
-    @profileQuiz.id = @current_user.profileQuiz_id
-    @profileQuiz = ProfileQuiz.find_by(id: @profileQuiz.id)
-    if attr_name == 'uber'
-      if @profileQuiz.answer1 == 1 || @profileQuiz.id == nil
-        return 5 if attr_value
-        return 1 if !attr_value
-      else
-        return 1 if attr_value
-        return 5 if !attr_value
+    if logged_in?
+      @profileQuiz = ProfileQuiz.new
+      @current_user = User.find_by(id: session[:user_id])
+      @profileQuiz.id = @current_user.profileQuiz_id
+      @profileQuiz = ProfileQuiz.find_by(id: @profileQuiz.id)
+      if attr_name == 'uber'
+        if @profileQuiz.answer1 == 1 || @profileQuiz.id == nil
+          return 5 if attr_value
+          return 1 if !attr_value
+        else
+          return 1 if attr_value
+          return 5 if !attr_value
+        end
       end
-    end
-    if attr_name == 'gini'
-      attr_value = 1 - attr_value
-    end
-    if attr_name == 'idh' || attr_name == 'gini'
-      if attr_value < 0.3
-        return 1
-      elsif (0.3...0.4).include?(attr_value)
-        return 2
-      elsif (0.4...0.6).include?(attr_value)
-        return 3
-      elsif(0.6..0.7).include?(attr_value)
-        return 4
-      elsif attr_value > 0.7
-        return 5
+      if attr_name == 'gini'
+        attr_value = 1 - attr_value
       end
-    end
+      if attr_name == 'idh' || attr_name == 'gini'
+        if attr_value < 0.3
+          return 1
+        elsif (0.3...0.4).include?(attr_value)
+          return 2
+        elsif (0.4...0.6).include?(attr_value)
+          return 3
+        elsif(0.6..0.7).include?(attr_value)
+          return 4
+        elsif attr_value > 0.7
+          return 5
+        end
+      end
 
-    @average = City.all.map(&attr_name.to_sym).inject(0, &:+)/City.all.length
+      @average = City.all.map(&attr_name.to_sym).inject(0, &:+)/City.all.length
+        if (0...@average*0.6).include?(attr_value)
+          if attr_name == 'population'
+            if @profileQuiz.answer4 == 1 then return 1 else return 5 end
+          end
+          if attr_name == 'area'
+            if @profileQuiz.answer3 == 1 then return 1 else return 5 end
+          end
+          if attr_name == 'demographic_density'
+            if @profileQuiz.answer2 == 1 then return 1 else return 5 end
+          end
+          if attr_name == 'health' then return 1 else return 5 end
+        elsif (@average*0.6...@average*0.9).include?(attr_value)
+          if attr_name == 'population'
+            if @profileQuiz.answer4 == 1 then return 2 else return 4 end
+          end
+          if attr_name == 'area'
+            if @profileQuiz.answer3 == 1 then return 2 else return 4 end
+          end
+          if attr_name == 'demographic_density'
+            if @profileQuiz.answer2 == 1 then return 2 else return 4 end
+          end
+          if attr_name == 'health' then return 2 else return 4 end
+        elsif ((@average*0.9...@average*1.1).include?(attr_value))
+          return 3
+        elsif ((@average*1.1...@average*1.4).include?(attr_value))
+          if attr_name == 'population'
+            if @profileQuiz.answer4 == 1 then return 4 else return 2 end
+          end
+          if attr_name == 'area'
+            if @profileQuiz.answer3 == 1 then return 4 else return 2 end
+          end
+          if attr_name == 'demographic_density'
+            if @profileQuiz.answer2 == 1 then return 4 else return 2 end
+          end
+          if attr_name == 'health' then return 4 else return 2 end
+        elsif attr_value > @average*1.4
+          if attr_name == 'population'
+            if @profileQuiz.answer4 == 1 then return 5 else return 1 end
+          end
+          if attr_name == 'area'
+            if @profileQuiz.answer3 == 1 then return 5 else return 1 end
+          end
+          if attr_name == 'demographic_density'
+            if @profileQuiz.answer2 == 1 then return 5 else return 1 end
+          end
+          if attr_name == 'health' then return 5 else return 1 end
+        end
+  else
+    if attr_name == 'uber'
+          return 5 if attr_value
+          return 1 if !attr_value
+        end
+      end
+      if attr_name == 'gini'
+        attr_value = 1 - attr_value
+      end
+      if attr_name == 'idh' || attr_name == 'gini'
+        if attr_value < 0.3
+          return 1
+        elsif (0.3...0.4).include?(attr_value)
+          return 2
+        elsif (0.4...0.6).include?(attr_value)
+          return 3
+        elsif(0.6..0.7).include?(attr_value)
+          return 4
+        elsif attr_value > 0.7
+          return 5
+        end
+      end
+      @average = City.all.map(&attr_name.to_sym).inject(0, &:+)/City.all.length
     if (0...@average*0.6).include?(attr_value)
-      if attr_name == 'population'
-        if @profileQuiz.answer4 == 1 then return 1 else return 5 end
+        if attr_name == 'health' then return 1 else return 5 end
+      elsif (@average*0.6...@average*0.9).include?(attr_value)
+        if attr_name == 'health' then return 2 else return 4 end
+      elsif ((@average*0.9...@average*1.1).include?(attr_value))
+        return 3
+      elsif ((@average*1.1...@average*1.4).include?(attr_value))
+        if attr_name == 'health' then return 4 else return 2 end
+      elsif attr_value > @average*1.4
+        if attr_name == 'health' then return 5 else return 1 end
       end
-      if attr_name == 'area'
-        if @profileQuiz.answer3 == 1 then return 1 else return 5 end
-      end
-      if attr_name == 'demographic_density'
-        if @profileQuiz.answer2 == 1 then return 1 else return 5 end
-      end
-      if attr_name == 'health' then return 1 else return 5 end
-    elsif (@average*0.6...@average*0.9).include?(attr_value)
-      if attr_name == 'population'
-        if @profileQuiz.answer4 == 1 then return 2 else return 4 end
-      end
-      if attr_name == 'area'
-        if @profileQuiz.answer3 == 1 then return 2 else return 4 end
-      end
-      if attr_name == 'demographic_density'
-        if @profileQuiz.answer2 == 1 then return 2 else return 4 end
-      end
-      if attr_name == 'health' then return 2 else return 4 end
-    elsif ((@average*0.9...@average*1.1).include?(attr_value))
-      return 3
-    elsif ((@average*1.1...@average*1.4).include?(attr_value))
-      if attr_name == 'population'
-        if @profileQuiz.answer4 == 1 then return 4 else return 2 end
-      end
-      if attr_name == 'area'
-        if @profileQuiz.answer3 == 1 then return 4 else return 2 end
-      end
-      if attr_name == 'demographic_density'
-        if @profileQuiz.answer2 == 1 then return 4 else return 2 end
-      end
-      if attr_name == 'health' then return 4 else return 2 end
-    elsif attr_value > @average*1.4
-      if attr_name == 'population'
-        if @profileQuiz.answer4 == 1 then return 5 else return 1 end
-      end
-      if attr_name == 'area'
-        if @profileQuiz.answer3 == 1 then return 5 else return 1 end
-      end
-      if attr_name == 'demographic_density'
-        if @profileQuiz.answer2 == 1 then return 5 else return 1 end
-      end
-      if attr_name == 'health' then return 5 else return 1 end
     end
-  end
 
   def get_hash_values
     @hashValue = Hash.new
