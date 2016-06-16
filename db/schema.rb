ad50a6d6b83b447d8e9c1c72b22e7f2476c4e266
MasterCaio <caiofelipe147@gmail.com>
2016-06-16 20:07:11 -0300

    Inserindo atributo de Estado na model de cidades, configurando para aparecer o estado da cidade ao passar o mouse sobre uma cidade Caio Nunes <caiofelipe147@gmail.com> Marcos Diego <marcos.dsg91@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529113751) do
+ActiveRecord::Schema.define(version: 20160616223523) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160529113751) do
     t.string   "image"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.string   "state"
   end
 
   create_table "comments", force: :cascade do |t|
