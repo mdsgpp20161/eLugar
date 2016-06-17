917088e2c2218e15890fc9078e664df2ca456873
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2016-06-16 21:52:59 -0300

    realizando alguns testes unitarios Signed-off-by: Harrison <hpedro1195@gmail.com> Signed-off-by: Gustavo <gustavo.ldbrito@gmail.com>

--- test/controllers/cities_controller_test.rb
+++ test/controllers/cities_controller_test.rb
@@ -2,11 +2,18 @@ require 'test_helper'
 
 class CitiesControllerTest < ActionController::TestCase
   fixtures :cities
+  include CitiesHelper
+  include SessionsHelper
 
   def setup
     @cityBrasilia = cities(:brasilia)
     @cityCeara = cities(:ceara)
     @cityGoiania = cities(:goiania)
+    @cityFlorianopolis = cities(:florianopolis)
+    @cityCuiaba = cities(:cuiaba)
+    @user = User.create(name: "Harrison", email: "pedro@gmail.com", 
+      password: "123456", password_confirmation: "123456")
+
   end
 
   test "should get index" do
@@ -36,8 +43,23 @@ class CitiesControllerTest < ActionController::TestCase
     assert @cityCeara.save
   end
 
-test "should get compare" do
-  get :compare, id: @cityBrasilia.id
+test "should get compare when searching city using search bar" do
+  get :compare, id: @cityBrasilia.id, newID: @cityCeara.id, find: 'bra'
+  assert_response :success
+end
+
+test "should get compare when user doesn't search a city" do
+  get :compare, id: @cityBrasilia.id, newID: @cityCeara.id
+  assert_response :success
+end
+
+test "should get ranking" do
+  get :ranking
+  assert_response :success
+end
+
+test "should show city using filter for population" do
+  get :show_cities, find: 'a', from_population: 3, to_population: 5
   assert_response :success
 end
 
@@ -91,29 +113,67 @@ test "should calculate valid average" do
   assert ((average - 33.333).abs > 0.0001)
 end
 
-test "get_emoji should return 1 when uber condition is false" do
-  controler = CitiesController.new
-  valor = controler.get_emoji('uber', !@cityBrasilia.uber)
+test "get_emoji_others should return 2 if params is health and be in some range" do
   
-  assert (valor == 1)
+  emoji = get_emoji_others('health', @cityFlorianopolis.health)
+  assert (emoji == 2)
+
 end
 
-test "get_emoji should return 3 when attr_name is health with some params" do
-  controler = CitiesController.new
-  @cityBrasilia.health = 5
-  valor = controler.get_emoji('health', @cityBrasilia.health)
+test "get_emoji_others should return 3 if params is health and be in some range" do
+  
+  emoji = get_emoji_others('health', @cityCuiaba.health)
+  assert (emoji == 3)
 
-  assert (valor == 3), "valor = #{valor}"
 end
 
+test "get_emoji_others should return 4 if params is health and be in some range" do
+  
+  emoji = get_emoji_others('health', @cityCeara.health)
+  assert (emoji == 4)
 
-test "get_emoji should return 5 when attr_name is health with some params" do
-  controler = CitiesController.new
-  valor = controler.get_emoji('health', @cityGoiania.health)
-
-  assert (valor == 5), "valor = #{valor}"
 end
 
+test "get_emoji_others should return 5 if params is health and be in some range" do
+  
+  emoji = get_emoji_others('health', @cityGoiania.health)
+  assert (emoji == 5)
+
+end
+
+test "should get emojis according to user preferences when he doesn't support uber" do
+    log_in(@user)
+
+    emoji = set_emojis_by_user('uber', 5)
+
+    assert (emoji == 1)
+end
+
+test "should get emojis according to user preferences when he likes crowded cities" do
+    log_in(@user)
+
+    emoji = set_emojis_by_user('demographic_density', 4)
+
+    assert (emoji == 2)
+end
+
+test "should get a valid type of return on valid_attributes_ranking " do 
+  param = valid_attributes_ranking
+
+  assert (param.class == Hash)
+end
+
+test "should get a valid type of return on valid_attributes_compare_down" do 
+  param = valid_attributes_compare_down
+
+  assert (param.class == Hash)
+end
+
+test "should get a valid type of return on valid_attributes_compare_up" do 
+  param = valid_attributes_compare_up
+
+  assert (param.class == Hash)
+end
 #test "should get compare from show" do
 #  get :compare, id: @cityBrasilia.id
 #  assert_response :success
